8366b4f7e3a0b24d37836ab18396479386bbfe8d
hafize.nur <hafizenurkorkmaz@gmail.com>
2021-01-17 21:29:14 +0300

    Add project files.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using BasicMvcExample.Models.Contact;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCINTROHNK.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ContactFormModel model)
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Contact/ContactFormModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BasicMvcExample.Models.Contact
+{
+    public class ContactFormModel
+    {
+        public int ContactFormId { get; set; }
+
+        [Required(ErrorMessage="Lütfen bir ad giriniz")]
+        public string NameSurname { get; set; }
+
+        [Display(Name ="Email")]
+        [Required(ErrorMessage = "Lütfen bir e-mail giriniz")]
+        [EmailAddress(ErrorMessage ="Email adresi geçersiz")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Lütfen bir konu giriniz")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Lütfen bir mesaj giriniz")]
+        public string Message { get; set; }
+    }
+}
